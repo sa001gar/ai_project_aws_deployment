efacf19f7354317229cc2b17270ccdf9acbb33af
sa001gar <kundusagar233@gmail.com>
2024-11-08 23:31:55 +0530

    Push

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -102,23 +102,23 @@ WSGI_APPLICATION = 'ai_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("db_name"),
-        'USER': os.getenv("db_username"),
-        'PASSWORD': os.getenv("db_password"),
-        'HOST': os.getenv("db_host"),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("db_name"),
+#         'USER': os.getenv("db_username"),
+#         'PASSWORD': os.getenv("db_password"),
+#         'HOST': os.getenv("db_host"),
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
